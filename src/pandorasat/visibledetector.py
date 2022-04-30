3a22c5aaa8b7aba5b33a13dbeb838e8503675d37
Tom Barclay <barclay.astro@gmail.com>
2022-04-30 16:13:05 -0400

    Update visibledetector.py

--- src/pandorasat/visibledetector.py
+++ src/pandorasat/visibledetector.py
@@ -7,5 +7,11 @@ from dataclasses import dataclass
 class VisibleDetector:
     """Holds information on the Visible Detector"""
 
+    # Detector Properties
+    npix_column: int = 2048 * u.pixel
+    npix_row: int = 2048 * u.pixel
+    pixel_scale: float = 0.78 * u.arcsec / u.pixel
+    pixel_size: float = 6.5 u.um / u.pixel
+    
     def __repr__(self):
         return "Pandora Visible Detector"
