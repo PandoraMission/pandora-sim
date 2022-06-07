64535a418a92ae1f18b7a4a5becca56af97e164d
Christina Hedges <christina.l.hedges@nasa.gov>
2022-06-07 10:48:10 -0700

    :sparkles: added visible PRF calculation

--- src/pandorasat/visibledetector.py
+++ src/pandorasat/visibledetector.py
@@ -38,16 +38,25 @@ class VisibleDetector:
         """
         raise NotImplementedError
 
-    def jitter(self, sigma=5, resolution=50):
+    def jitter(self, xstd=4, ystd=1.5, tstd=3, ntimes=50, seed=42):
         """Returns the jitter inside a cadence
 
         This is a dumb placeholder function.
         """
-        jitter_x = convolve(
-            np.random.normal(0, sigma, size=resolution), Gaussian1DKernel(3)
+        np.random.seed(seed)
+        jitter_x = (
+            convolve(np.random.normal(0, xstd, size=ntimes), Gaussian1DKernel(tstd))
+            * tstd ** 0.5
+            * xstd ** 0.5
         )
-        jitter_y = convolve(
-            np.random.normal(0, sigma, size=resolution), Gaussian1DKernel(3)
+        np.random.seed(seed + 1)
+        jitter_y = (
+            convolve(
+                np.random.normal(0, ystd, size=ntimes),
+                Gaussian1DKernel(tstd),
+            )
+            * tstd ** 0.5
+            * ystd ** 0.5
         )
         return jitter_x, jitter_y
 
@@ -57,12 +66,17 @@ class VisibleDetector:
         # This is from Tom, I'm assuming the units are pixels, should check with him
         x = np.arange(-256, 257) * np.ravel(data["dx"]) / 6.5 * u.pixel
         y = np.arange(-256, 257) * np.ravel(data["dx"]) / 6.5 * u.pixel
-        psf /= np.trapz(np.trapz(psf, x.value, axis=0), y.value)
+
+        # Tom thinks this step shoudl be done later when it's at the "science" level...
+        psf /= np.trapz(np.trapz(psf, x.value, axis=1), y.value)
 
         xnew = np.arange(-33.5, 33.5, 0.04)
         ynew = np.arange(-33.5, 33.5, 0.04)
         f = interpolate.interp2d(x, y, np.log(psf), kind="cubic")
         psf = np.exp(f(xnew, ynew))
+
+        # This should be convolved with a Gaussian for high frequency noise.
+
         # Making PSF wavelength dependent in a fake way for now
         wavelength = ((50 * (np.arange(0, 11) - 5)) + 450) * u.nm
         w = wavelength.to(u.nm).value / 450 - 1
@@ -75,6 +89,7 @@ class VisibleDetector:
             wavelength,
             psf_cube,
         )
+        # The PSF should probably be convolved with a Gaussian to account for high frequency noise.
 
     def _bin_prf(self, center=(0, 0)):
         mod = (self.psf_x.value + center[0]) % 1
@@ -89,10 +104,33 @@ class VisibleDetector:
         psf2 = np.asarray(
             [psf1[:, cyc == c].sum(axis=1) / (cyc == c).sum() for c in ybin]
         )
+
+        # We need to renormalize psf2 here
+        psf2 /= np.asarray(
+            [
+                np.trapz(np.trapz(psf2[:, :, idx], xbin, axis=1), ybin)
+                for idx in range(psf2.shape[2])
+            ]
+        )[None, None, :]
         return xbin.astype(int), ybin.astype(int), psf2
 
-    def prf(self, center=(0, 0), sigma=5):
-        jitter_x, jitter_y = self.jitter(sigma=sigma)
+    def prf(self, center=(0, 0), seed=42, xstd=4.5, ystd=1, tstd=3, ntimes=50):
+        """
+        Make a Pixel Response Function
+
+        Parameters
+        ----------
+        xstd: np.ndarray
+            Standard deviation of the jitter in X dimension
+        ystd: np.ndarray
+            Standard deviation of the jitter in Y dimension
+        tstd: np.ndarray
+            Width of Gaussian convolution in the time dimension. Higher
+            values will give longer time correlations in jitter.
+        """
+        jitter_x, jitter_y = self.jitter(
+            ntimes=ntimes, xstd=xstd, ystd=ystd, tstd=tstd, seed=seed
+        )
         jitter_x += center[0]
         jitter_y += center[1]
 
@@ -116,8 +154,15 @@ class VisibleDetector:
             X, Y = np.asarray(np.meshgrid(x - xmin, y - ymin))
             res[X, Y] += prf
         res /= len(prfs)
-        return (
+        x, y = (
             np.arange(np.hstack(xs).max() - xmin + 1) + xmin,
             np.arange(np.hstack(ys).max() - ymin + 1) + ymin,
+        )
+        res /= np.asarray([np.sum(res[:, :, idx]) for idx in range(res.shape[2])])[
+            None, None, :
+        ]
+        return (
+            x,
+            y,
             res,
         )
