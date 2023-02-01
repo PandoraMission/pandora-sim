6eb5f2be9b3f8ac20a1a3b9b76f246b33eafa1df
Christina Hedges <christina.l.hedges@nasa.gov>
2023-02-01 16:40:49 -0500

    added download on import

--- src/pandorasat/__init__.py
+++ src/pandorasat/__init__.py
@@ -4,6 +4,34 @@ import os  # noqa
 
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
+# Standard library
+import logging  # noqa: E402
+import shutil  # noqa: E402
+
+# Third-party
+from astropy.utils.data import download_file  # noqa: E402
+
+logging.basicConfig()
+logger = logging.getLogger("pandorasat")
+
+if not os.path.isfile(f"{PACKAGEDIR}/data/pandora_vis_20220506.fits"):
+    # Download vis PSF
+    logger.warning("No PSF file found. Downloading 100MB VIS PSF file.")
+    p = download_file(
+        "https://zenodo.org/record/7596336/files/pandora_vis_20220506.fits?download=1",
+        pkgname="pandora-sat",
+    )
+    shutil.move(p, f"{PACKAGEDIR}/data/pandora_vis_20220506.fits")
+
+if not os.path.isfile(f"{PACKAGEDIR}/data/pandora_nir_20220506.fits"):
+    # Download nir PSF
+    logger.warning("No PSF file found. Downloading 10MB NIR PSF")
+    p = download_file(
+        "https://zenodo.org/record/7596336/files/pandora_nir_20220506.fits?download=1",
+        pkgname="pandora-sat",
+    )
+    shutil.move(p, f"{PACKAGEDIR}/data/pandora_nir_20220506.fits")
+
 from .pandorasat import PandoraSat  # noqa
 from .psf import PSF  # noqa
 from .targets import Target  # noqa
--- tests/test_pandora_sat.py
+++ tests/test_pandora_sat.py
@@ -1,5 +1,6 @@
 # Third-party
 import astropy.units as u
+from astropy.coordinates import SkyCoord
 import numpy as np
 import pytest
 
@@ -57,3 +58,39 @@ def test_psf():
     nirPSF.prf((1, 10), location=(1, 3))
     x, y, prf = nirPSF.prf(nirPSF.midpoint, location=(0, 0))
     np.isclose(np.trapz(np.trapz(prf, x, axis=0), y, axis=0), 1, atol=1e-5)
+
+
+@pytest.mark.remote_data
+def test_visiblesim():
+    # Set up the "observatory"
+    p = PandoraSat()
+    # Set the target name to any from the target list.
+    targetname = "TRAPPIST-1"
+    c = SkyCoord.from_name(targetname)
+    # Get a list of sources nearby that will be on the detector
+    source_catalog = p.get_sky_catalog(c.ra.deg, c.dec.deg)
+    # Set up jitter properties
+    xjitter_3sigma = 2*u.pix
+    yjitter_3sigma = 2*u.pix
+    jitter_timescale = 1*u.second
+    # 40 frames
+    nt = 40
+    prf_func = p.VISDA.get_fastPRF(wavelength=0.54*u.micron, temperature=10*u.deg_C)
+    # Build the images
+    time, xcenter, ycenter, science_images = p.get_sky_images(source_catalog,
+                                                            nreads=1,
+                                                            nt=nt, 
+                                                            xjitter_3sigma=xjitter_3sigma,
+                                                            yjitter_3sigma=yjitter_3sigma, 
+                                                            jitter_timescale=jitter_timescale,
+                                                            prf_func=prf_func)
+    assert isinstance(science_images, u.Quantity)
+    assert isinstance(time, u.Quantity)
+    assert isinstance(xcenter, u.Quantity)
+    assert isinstance(ycenter, u.Quantity)
+    assert science_images.shape == (40, 2048, 2048)
+    assert time.shape == (40,)
+    assert xcenter.shape == (40,)
+    assert ycenter.shape == (40,)
+    assert np.abs(xcenter).max().value < 10
+    assert np.abs(ycenter).max().value < 10
