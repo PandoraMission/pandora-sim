f2e75a12354243f2e8e584716f3c66545efaf8bf
benhord <benjhord@gmail.com>
2024-03-07 17:50:57 -0500

    Updated version number test to new version

--- tests/test_pandora_sim.py
+++ tests/test_pandora_sim.py
@@ -8,7 +8,7 @@ from pandorasim import PACKAGEDIR, PSF, PandoraSim, Target, __version__
 
 
 def test_version():
-    assert __version__ == "0.5.0"
+    assert __version__ == "0.5.1"
 
 
 def test_pandorasim():
